f149d4335c80494f0a15d1b561f5ad036667400e
felipevelandia06 <141078736+felipevelandia06@users.noreply.github.com>
2023-09-03 06:26:44 +0000

    avance

--- taller.py
+++ taller.py
@@ -25,46 +25,46 @@ elif 40<altura<=60 and 25<peso<=45:
 else: print('Su perro mestizo no tiene medidas para catalogarlo en un tamaño específico')'''
 
 #ejercicio3
-def menu():
-  print('''Ingresa el codigo correspondiente a la conversión que quiera realizar 
-1. °C - °F
-2. °F - °C
-3. °C - °K
-4. °K - °C
-5. °F - °K
-6. °k - °F''')
+'''def menu():
+  #print('Ingresa el codigo correspondiente a la conversión que quiera realizar')
+#1. °C - °F
+#2. °F - °C
+#3. °C - °K
+#4. °K - °C
+#5. °F - °K
+#6. °k - °F)'''
 
-def cf(a):
-  t=(a*9/5)+32
-  print(t)
-  return t
+#def cf(a):
+  #t=(a*9/5)+32
+  #print(t)
+  #return t'''
 
-def fc(a):
-  t=(a-32)*5/9
-  print(t)
-  return t
+#def fc(a):
+  #t=(a-32)*5/9
+  #print(t)
+  #return t'''
 
-def ck(a):
+'''def ck(a):
   t=a+273.15
   print(t)
-  return t
+  return t'''
 
-def kc(a):
+'''def kc(a):
   t=a-273.15
   print(t)
-  return t
+  return t'''
 
-def fk(a):
+'''def fk(a):
   t=(a-32)*5/9+273.15
   print(t)
-  return t
+  return t'''
 
-def kf(a):
+'''def kf(a):
   t=(a-273.15)*9/5+32
   print(t)
-  return t
+  return t'''
 
-def principal():
+'''def principal():
   menu()
   codigo=int(input())
   temperatura=float(input('ingrese la temperatura que quiera convertir '))
@@ -80,6 +80,49 @@ def principal():
     fk(temperatura)
   elif codigo==6:
     kf(temperatura)
-  else: print('código no valido')
+  else: print('código no valido')'''
 
-principal()
+'''principal()'''
+
+#ejercicio4
+'''def menores(a):
+  print('el costo de su curso es de 25.000 (grupo menores)')
+  if 10<=a<=13:
+    d=25000-(25000*0.15)
+    print('el costo de su curso con el descuento es de', d)
+  elif a==17:
+    d=25000-(25000*0.08)
+    print('el costo de su curso con el descuento es de', d)
+  else: print('no aplica para ningun descuento')'''
+
+'''def adultos(a):
+  print('el costo de su curso es de 35.000 (grupo adultos)')
+  if 18<=a<=30:
+    d=35000-(35000*0.11)
+    print('el costo de su curso con el descuento es de', d)
+  else: 
+    d=35000-(35000*0.09)
+    print('el costo de su curso con el descuento es de', d)'''
+
+'''def mayores(a):
+  print('el precio de su curso es de 50.000 (grupo adultos mayores)')
+  if a>65:
+    d=50000-(50000*0.4)
+    print('el costo de su curso con el descuento es de', d)
+  else: print('no aplica para ningun descuento')'''
+
+'''def principal():
+  n=input('ingrese su nombre ')
+  edad=int(input('ingrese su edad '))
+  if 10<=edad<=17:
+    menores(edad)
+  elif 18<=edad<=50:
+    adultos(edad)
+  elif edad>50:
+    mayores(edad)
+  else: 
+    print('no tiene edad permitida para inscribirse')'''
+
+'''principal()'''
+
+#ejercicio5
